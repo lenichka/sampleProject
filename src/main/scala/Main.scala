5c712b193ffd4d2085273390f3e993bec9177a80
Helen Prask <helen.prask@gmail.com>
2016-04-09 15:27:19 -0700

    Initial Commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,21 @@
 
 
 object Main {
-
-  def time[A](f: => A) = {
-    val s = System.nanoTime
-    val ret = f
-    println("time: "+(System.nanoTime-s)/1e6+"ms")
-    ret
+  /** Time difference since start in ms
+    *
+    * @param start Start time to calculate delta in ms
+    * @return milliseconds passed since start
+    */
+  def timeSince(start: Long): Long = {
+    ((System.nanoTime-start)/1e6).toLong
   }
 
 
   def main(args: Array[String]) {
     val start = System.nanoTime
-    printf("Version %2d.%3d %s\n", 0, 1, "You know, for starters.")
 
-    time(System.nanoTime - start)
+    printf("Version %2d.%d %s\n", 0, 1, "You know, for starters.")
+
+    println("time: "+ timeSince(start) +"ms")
   }
 }
--- src/test/scala/SampleSpec.scala
+++ src/test/scala/SampleSpec.scala
@@ -2,8 +2,14 @@ import org.scalatest._
 
 
 class SampleSpec extends FlatSpec with Matchers {
+// See manual here http://doc.scalatest.org/1.8/org/scalatest/FlatSpec.html
 
   "A Sample object" should "do nothing" in {
+    val sample = new Sample()
+    sample should be ( sample )
+  }
+
+  it should "do nothing else" in {
     val sample = new Sample()
     sample should be ( sample )
   }
